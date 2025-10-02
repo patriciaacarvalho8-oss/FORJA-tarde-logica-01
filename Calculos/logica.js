689b4d37df54d0829765aec34fcb4a111cbb15e5
patriciaacarvalho8-oss <patricia.a.carvalho8@aluno.senai.br>
2025-10-02 16:51:28 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -7,16 +7,35 @@ function soma(){
     resposta.textContent = numero1 + numero2
 
     // console.log(numero1, numero2)
-
-
 }
 
 
 function sub(){
-     var numero1 = document.getElementById("n3").valueAsNumber
-    var numero2 = document.getElementById("n4").valueAsNumber
-
-    var resposta = document.getElementById("respSub")
-
-    resposta.textContent = numero1 - numero2
+    var num1 = parseFloat(document.getElementById("n3").value)
+     var num2 = parseFloat(document.getElementById("n4").value)
+     document.getElementById("respSub").textContent = num1 - num2
 }
+
+function mult(){
+    var number1 = parseFloat(document.getElementById("n5").value)
+     var number2 = parseFloat(document.getElementById("n6").value)
+     document.getElementById("respMulti").textContent = number1 * number2
+}
+
+ function div(){
+   var a = parseFloat(document.getElementById("n7").value)
+     var b = parseFloat(document.getElementById("n8").value)
+     var resp = document.getElementById("respDiv")
+
+     if(b!== 0){
+        resp.innerHTML = (a / b).toFixed(2)
+     }else{
+        resp.innerHTML = "não se divide por 0"
+     }
+    
+
+      
+ }
+
+
+
